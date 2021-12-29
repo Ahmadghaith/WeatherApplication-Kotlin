0e6a8e018dd3c2d3879b04de7c71d99685ccbb03
Ahmad Ghaith <ahmadghaith14@yahoo.com>
2021-12-29 18:02:51 +0200

    Added previous changes from old project

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.weatherapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Navigation
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bott_nav)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
