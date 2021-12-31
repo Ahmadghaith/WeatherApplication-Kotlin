6f75bd623b1bf81a01a9bf8e54fbdbe5e0e7da95
Ahmad Ghaith <ahmadghaith14@yahoo.com>
2021-12-31 19:50:43 +0200

    Added part of the api call (Not fully working)

---
+++ app/src/main/java/com/example/weatherapplication/API/WeatherApi.kt
@@ -0,0 +1,10 @@
+package com.example.weatherapplication.API
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface WeatherApi {
+
+    @GET("?q=beirut&appid=41afd91e8508faf248e58bef14ffea2d&units=metric")
+    fun getWeatherByCity(): Call<String>
+}
--- app/src/main/java/com/example/weatherapplication/fragments/TodayFragment.kt
+++ app/src/main/java/com/example/weatherapplication/fragments/TodayFragment.kt
@@ -6,10 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import android.widget.TextView
+import android.widget.Toast
+import com.example.weatherapplication.API.WeatherApi
 import com.example.weatherapplication.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.DisposableHandle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class TodayFragment : Fragment() {
+
+    private val baseUrl = "https://api.openweathermap.org/data/2.5/weather/"
+    val apiKey = "41afd91e8508faf248e58bef14ffea2d"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,12 +38,32 @@ class TodayFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_today, container, false)
 
         val search = view.findViewById<SearchView>(R.id.search)
+        val text = view.findViewById<TextView>(R.id.textView)
 
+
+        GlobalScope.launch(Dispatchers.Main){
         //Retrofit instance
-        /*val rertofit = Retrofit.Builder()
-            .baseUrl("https://api.openweathermap.org/data/2.5/weather")
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
-*/
+        val weatherApi = retrofit.create(WeatherApi::class.java)
+
+        //OpenWeatherApi call
+        val result = weatherApi.getWeatherByCity()
+        result.enqueue(object : Callback<String>
+        {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if(response.isSuccessful) {
+                    text.text = response.body()
+                }
+            }
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Toast.makeText(activity,"Error",Toast.LENGTH_SHORT).show();
+
+            }
+        })}
+
         return view
     }
 }
